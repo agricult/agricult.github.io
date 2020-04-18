b4bbae6d112dbfc64592e749fe5b5b588c96f6bb
agricult <banarasi0418@gmail.com>
2020-04-18 14:40:39 +0530

    deploying website

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+    return (
+        <div className='contact'>
+            <h1>Contact Details</h1>
+            <h2>Agricultural Price Prediction</h2>
+            <h3>Bangalore, India</h3>
+            <h3>Email: <a href="mailto:badrinath.mn@gmail.com?Subject=Agricultural%20%Price%20Prediction%20Enquiry" target="_top">badrinath.mn@gmail.com</a></h3>
+        </div>
+    );
+}
+
+export default Contact;
