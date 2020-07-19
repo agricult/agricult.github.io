6c20777c255194a0d6b7870e586cc05e93ea62c7
badrinan <badrinan@netapp.com>
2020-07-18 19:28:12 -0700

    Updates

---
+++ precache-manifest.47e767c347efc39225e258f3bdd0589a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557482451686330292d31e28ea875fef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81b7142cb98ea973d02f",
+    "url": "/static/css/main.987f66c0.chunk.css"
+  },
+  {
+    "revision": "ab55101f004595c102fe",
+    "url": "/static/js/2.ed144006.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/static/js/2.ed144006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81b7142cb98ea973d02f",
+    "url": "/static/js/main.04be534b.chunk.js"
+  },
+  {
+    "revision": "2bf176c09f7fd440bc54",
+    "url": "/static/js/runtime-main.24f834b5.js"
+  },
+  {
+    "revision": "6f5ae2b6ae6a173cf53374b6bc58e707",
+    "url": "/static/media/agri-india-3.6f5ae2b6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f46d780c438c42fc26d5d2549d589af7.js"
+  "/precache-manifest.47e767c347efc39225e258f3bdd0589a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
